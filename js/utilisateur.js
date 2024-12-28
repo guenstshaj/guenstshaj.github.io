2fdea726c99be9049244cd1633a4ac3fadaf1d0c
Guens Tshaj <90930975+guenstshaj@users.noreply.github.com>
2024-12-28 20:51:52 +0300

    Update utilisateur.js

--- js/utilisateur.js
+++ js/utilisateur.js
@@ -82,7 +82,7 @@ users.push(new Utilisateur("Monsieur Guens","Membre", [5000]));
 users.push(new Utilisateur("Eli","Membre", [5000]));
 users.push(new Utilisateur("Gigie","Membre", [5000]));
 users.push(new Utilisateur("Johnson","Membre", [5000]));
-users.push(new Utilisateur("Eric","Invité", [3000]));
+users.push(new Utilisateur("Eric","Invité", [3000,2000]));
 
 
 // Appeler la fonction pour afficher les utilisateurs
